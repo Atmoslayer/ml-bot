af5d5f684f506d8d452e51143ef497ec59c791fd
Atmoslayer <ddsomdim@gmail.com>
2023-03-16 23:35:34 +0300

    Add DialogFlow to VK bot

--- vk_bot.py
+++ vk_bot.py
@@ -6,11 +6,16 @@ import random
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+from main import detect_intent_texts
 
-def echo(event, vk_api):
+
+def reply(event, vk_api, project_id):
+    chat_id = event.user_id
+    reply_text = detect_intent_texts(project_id, chat_id, event.text)
+
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=reply_text,
         random_id=random.randint(1,1000)
     )
 
@@ -19,6 +24,9 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     load_dotenv()
     vk_token = os.getenv('VK_TOKEN')
+    google_token = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+    google_cloud_project = os.getenv('GOOGLE_CLOUD_PROJECT')
+    project_id = os.getenv('PROJECT_ID')
 
     vk_session = vk_api.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
@@ -26,4 +34,4 @@ if __name__ == '__main__':
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            reply(event, vk_api, project_id)
