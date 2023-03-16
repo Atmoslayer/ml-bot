4692f003724eca12a64eac3942387ec80589321f
Atmoslayer <ddsomdim@gmail.com>
2023-03-16 23:28:23 +0300

    Echobot for VK

--- vk_bot.py
+++ vk_bot.py
@@ -1,9 +1,19 @@
 import vk_api
 import os
 import logging
+import random
 
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1,1000)
+    )
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
@@ -11,14 +21,9 @@ if __name__ == '__main__':
     vk_token = os.getenv('VK_TOKEN')
 
     vk_session = vk_api.VkApi(token=vk_token)
-
+    vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            logging.info('Новое сообщение:')
-            if event.to_me:
-                logging.info(f'Для меня от: {event.user_id}')
-            else:
-                logging.info(f'От меня для: {event.user_id}')
-            logging.info(f'Текст: {event.text}')
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
